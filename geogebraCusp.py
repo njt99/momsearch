423c154f72e5e3fb02e1cf571ec8079277e11a9f
TimeMemorial <redgod@gmail.com>
2015-10-22 22:29:05 -0400

    Got geogebra output working properly and printing parameters to system err

--- geogebraCusp.py
+++ geogebraCusp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import os, sys, glob, pprint
+import os, sys, glob
+from pprint import *
 from numpy import *
 
 scale = map(lambda x : 8 * pow(2, x/6.), range(0,-6,-1))
@@ -78,8 +79,14 @@ def get_first(word) :
     else :
         return ''
 
+if len(sys.argv) < 3 :
+    print 'Usage: geogebraCusp box_code depth <height_cutoff>'
+    sys.exit(2)
+
 box = sys.argv[1]
 depth = int(sys.argv[2])
+height_cutoff = float(sys.argv[3]) if len(sys.argv) > 3 else 0.
+
 params = get_center(box)
 G = get_G(params)
 g = get_g(params)
@@ -91,10 +98,10 @@ I = [[1.,0.],[0.,1.]]
 horoballs = { 0 : { '' : { 'center' : 0, 'height' : cusp_height, 'gamma' : I, 'word' : ''}}}
 
 # Print box paramenters and group elements
-#print 'Parameters:'
-#pprint.pprint(params,width=1)
-#print 'Elemnts:'
-#pprint.pprint(elements,width=2)
+sys.stderr.write('Parameters:\n')
+sys.stderr.write(pformat(params,width=1)+'\n')
+sys.stderr.write('Elemnts:\n')
+sys.stderr.write(pformat(elements,width=2)+'\n')
 
 # Generate new horoballs
 d = 0
@@ -134,29 +141,38 @@ while d < depth :
             # Keep track of the word and it's representative
             new_word = h_char + word
             new_gamma = dot(h,gamma)
-            M_word = 'M'*M_pow if M_pow > 0 else 'm'*M_pow 
-            new_word = M_word + new_word
-            N_word = 'N'*N_pow if N_pow > 0 else 'n'*N_pow 
-            new_word = N_word + new_word
-            T = get_T(params, M_pow, N_pow)
-            new_gamma = dot(T, new_gamma)
-            new_center = mobius(T, h_center)
+            if new_height > 2*cusp_height :
+                sys.stderr.write('Possible giant horoball with word {0} of height {1} with center {2}\nElement:\n'.format(new_word,new_height,new_center))
+                sys.stderr.write(pformat(new_gamma,width=2)+'\n')
+            else :
+                M_word = 'M'*M_pow if M_pow > 0 else 'm'*M_pow 
+                new_word = M_word + new_word
+                N_word = 'N'*N_pow if N_pow > 0 else 'n'*N_pow 
+                new_word = N_word + new_word
+                T = get_T(params, M_pow, N_pow)
+                new_gamma = dot(T, new_gamma)
+                new_center = mobius(T, h_center)
 
-            horoballs[d+1][new_word] = { 'center' : new_center, 'height' : new_height, 'gamma' : new_gamma, 'word' : new_word }
+                horoballs[d+1][new_word] = { 'center' : new_center, 'height' : new_height, 'gamma' : new_gamma, 'word' : new_word }
     d += 1
 
 #pprint.pprint(horoballs,width=2)
 
+sphere_count = 0
 command = 'Execute[{'
 for d in horoballs :
     depth_level = horoballs[d]
     for word in depth_level :
         ball = depth_level[word]
-        base = ball['center']
-        rad = ball['height']/2.
-        center = (float(real(base)),float(imag(base)),rad)
-        command += '\"Sphere[({0:.20f},{1:.20f},{2:.20f}),{3:.20f}]\",'.format(center[0],center[1],center[2],rad)
+        height = ball['height']
+        if height > height_cutoff :
+            base = ball['center']
+            rad = height/2.
+            center = (float(real(base)),float(imag(base)),rad)
+            command += '\"Sphere[({0:.20f},{1:.20f},{2:.20f}),{3:.20f}]\",'.format(center[0],center[1],center[2],rad)
+            sphere_count += 1
 
+sys.stderr.write('Generated {0} spheres\n'.format(sphere_count))
 command = command[:-1]
 command += '}]'
 print command
