0d65d155ea4d2a41dc7c8654a7309e05cc77d768
Andrew Yarmola <yarmola@bc.edu>
2016-01-21 16:55:25 -0500

    Latest data update. All holes have quasirelators. TODO: implement Meyerhoff box size check

--- holes_task.cmd
+++ holes_task.cmd
@@ -2,8 +2,8 @@
 # serial job using 1 node and 8 processor,
 # and runs for 24 hours (max).
 #SBATCH -N 1 # node count
-#SBATCH --ntasks-per-node=8
-#SBATCH -t 120:00:00
+#SBATCH --ntasks-per-node=4
+#SBATCH -t 23:59:00
 # sends mail when process begins, and 
 # when it ends. Make sure you define your email 
 #SBATCH --mail-type=begin
@@ -11,16 +11,16 @@
 #SBATCH --mail-user=yarmola@princeton.edu
 
 
-bin_dir='/Users/yarmola/Mathematics/Research/MOM2015/momsearch'
+bin_dir="/home/ayarmola/momsearch"
 words_dir=$bin_dir
 
 search="$bin_dir/dosearch.py"
 words="$words_dir/words"
 powers="$words_dir/powers_combined"
-holes="$bin_dir/holes_to_run"
+holes="$bin_dir/open_holes"
 
-data_dir='/Volumes/Tosha/validate_run'
+data_dir="/scratch/network/ayarmola/run2015"
 
 cd $bin_dir
 
-python "$search" -w "$words" -p "$powers" -c 8 -d 67 -h "$holes" "$data_dir/source" "$data_dir/output" > "$data_dir/holes.log" 2>&1
+python "$search" -w "$words" -p "$powers" -c 12 -d 65 -h "$holes" "$data_dir/source" "$data_dir/output" > "$data_dir/holes.log" 2>&1
