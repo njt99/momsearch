95a5705ee6abd5deba1a7f0bd48757daa56eb995
Andrew Yarmola <yarmola@bc.edu>
2016-01-24 07:22:47 -0500

    Updated search_task to newest format

--- search_task.cmd
+++ search_task.cmd
@@ -1,14 +1,27 @@
 #!/bin/bash
-# serial job using 1 node and 4 processor,
+# serial job using 1 node and 8 processor,
 # and runs for 24 hours (max).
 #SBATCH -N 1 # node count
 #SBATCH --ntasks-per-node=4
-#SBATCH -t 120:00:00
+#SBATCH -t 23:59:00
 # sends mail when process begins, and 
 # when it ends. Make sure you define your email 
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=yarmola@princeton.edu
 
-cd /home/ayarmola/momsearch
-python dosearch.py -w /home/ayarmola/momsearch/words -c 16 -d 67 /scratch/network/ayarmola/run2015/source /scratch/network/ayarmola/run2015/output > /home/ayarmola/run2015.log 2>&1
+
+bin_dir="/home/ayarmola/momsearch"
+#bin_dir="/Users/yarmola/Mathematics/Research/MOM2015/momsearch"
+words_dir=$bin_dir
+
+search="$bin_dir/dosearch.py"
+words="$words_dir/words"
+powers="$words_dir/powers_combined"
+
+data_dir="/scratch/network/ayarmola/run2015"
+#data_dir="$bin_dir/refine_census"
+
+cd $bin_dir
+
+python "$search" -w "$words" -p "$powers" -c 12 -d 70 "$data_dir/source" "$data_dir/output" > "$data_dir/search.log" 2>&1
