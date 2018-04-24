2c27db7037b7f1d47298b48eccb0bc398c828a14
Andrew Yarmola <andrew.yarmola@gmail.com>
2018-04-24 18:07:56 -0400

    Fixed segfault in Ball Search, added more comments

--- src/BallSearch.cpp
+++ src/BallSearch.cpp
@@ -25,7 +25,7 @@ namespace BallSearchImpl {
 	};
 	
 	struct LatticePoint {
-		Params<XComplex>* params;
+		Params<XComplex> params;
 		int x;
 		int y;
 		double distance;
@@ -53,11 +53,11 @@ namespace BallSearchImpl {
 	Word operator * (const Word& lhs, const Word& rhs); // Multiply/concatiante two words
 	
 	struct WordPair {
-		WordPair(Word* first, Word* second, Params<XComplex>* params);
+		WordPair(Word& first, Word& second, Params<XComplex>& params);
 		void setCurrentIndex(int i);
 		
-		Word* firstWord;
-		Word* secondWord;
+		Word firstWord;
+		Word secondWord;
 		vector<LatticePoint> distances;
 		int distanceIndex;
 		Word currentCombination;
@@ -86,7 +86,7 @@ namespace BallSearchImpl {
 		CanonicalName canonicalName;
 		SL2C G;
 		SL2C g;
-		list<Word> words;
+		vector<Word> words;
 		priority_queue<WordPair, vector<WordPair>, SmallerBallSize> pairs;
 		bool m_foundBigBall;
 	};
@@ -129,7 +129,7 @@ namespace BallSearchImpl {
 		for (int i = 0; i < -rhs.x; ++i) result.name += "m";
 		for (int i = 0; i <  rhs.y; ++i) result.name += "N";
 		for (int i = 0; i < -rhs.y; ++i) result.name += "n";
-		result.matrix = lhs.matrix * constructT(*rhs.params, rhs.x, rhs.y);
+		result.matrix = lhs.matrix * constructT(rhs.params, rhs.x, rhs.y);
 		return result;
 	}
 	
@@ -141,28 +141,36 @@ namespace BallSearchImpl {
 		return result;
 	}
 
-	WordPair::WordPair(Word* first, Word* second, Params<XComplex>* params)
+	WordPair::WordPair(Word& first, Word& second, Params<XComplex>& params)
 		:firstWord(first), secondWord(second), distanceIndex(0)
 	{
-		XComplex centerDiff = (first->matrix.a / first->matrix.c
-		 - second->matrix.a / second->matrix.c).z;
-		int y0 = int(floor(centerDiff.im / params->lattice.im));
-		int x0 = int(floor(centerDiff.re - y0*params->lattice.re));
-		for (int x = -2-x0; x <= 3-x0; ++x) {
-			for (int y = -1-y0; y <= 2-y0; ++y) {
-                // this is here to avoid having to reduce first^(-1) * second
-				if (x == 0 && y == 0 && firstWord->name[0] == secondWord->name[0])
-					continue;
-				LatticePoint l;
-				l.params = params;
-				l.x = -x;
-				l.y = -y;
-				XComplex t = (centerDiff + double(x) + double(y)*params->lattice).z;
-				l.distance = pow(absUB(t),2);
-				if (abs(l.x) < 4 && abs(l.y) < 4) 
-					distances.push_back(l);
-			}
-		}
+//        fprintf(stderr, "Buidling word pair\n");
+//        fprintf(stderr, "a_1 = %f + %f i, c_1 = %f + %f i\n a_2 =  %f + %f i, c_2 = %f + %f i\n",
+//                        firstWord.matrix.a.re, firstWord.matrix.a.im, firstWord.matrix.c.re, firstWord.matrix.c.im,
+//                        secondWord.matrix.a.re, secondWord.matrix.a.im, secondWord.matrix.c.re, secondWord.matrix.c.im); 
+		XComplex centerDiff = (firstWord.matrix.a / firstWord.matrix.c
+		 - secondWord.matrix.a / secondWord.matrix.c).z;
+        if (absUB(centerDiff) < infinity()) {
+            int y0 = int(floor(centerDiff.im / params.lattice.im));
+            int x0 = int(floor(centerDiff.re - y0*params.lattice.re));
+//            fprintf(stderr, "%d, %d\n", x0, y0); 
+            for (int x = -2-x0; x <= 3-x0; ++x) {
+                for (int y = -1-y0; y <= 2-y0; ++y) {
+                    // this is here to avoid having to reduce first^(-1) * second
+                    if (x == 0 && y == 0 && firstWord.name[0] == secondWord.name[0])
+                        continue;
+                    LatticePoint l;
+                    l.params = params;
+                    l.x = -x;
+                    l.y = -y;
+                    XComplex t = (centerDiff + double(x) + double(y)*params.lattice).z;
+                    l.distance = pow(absUB(t),2);
+//                    fprintf(stderr, "%d, %d\n", -x, -y); 
+                    if (abs(l.x) < 4 && abs(l.y) < 4) 
+                        distances.push_back(l);
+                }
+            }
+        }
 		sort(distances.begin(), distances.end(), LowerDistance());
 		setCurrentIndex(0);
 	}
@@ -173,7 +181,7 @@ namespace BallSearchImpl {
 		if (i >= distances.size()) {
 			currentBallSize = 0;
 		} else {
-			currentCombination = (inverse(*firstWord) * distances[i]) * (*secondWord);
+			currentCombination = (inverse(firstWord) * distances[i]) * secondWord;
             // Looks like this is an heuristic ball size 
 			currentBallSize = 0.5 / pow(absLB(currentCombination.matrix.c),2);
 		}
@@ -185,52 +193,72 @@ namespace BallSearchImpl {
 	
 	void BallSearch::pushWord(Word word)
 	{
-		list<Word>::iterator it;
+		vector<Word>::iterator it;
 		// first, check to see if this word has already been pushed
 		findNames(word);
-		if (word.nameClass == "")
+		if (word.nameClass == "") {
 			return;
-		for (it = words.begin(); it != words.end(); ++it)
-			if (it->nameClass == word.nameClass)
+        }
+		for (it = words.begin(); it != words.end(); ++it) {
+			if (it->nameClass == word.nameClass) {
 				return;
-		//printf("pushWord(%s) class=%s\n", word.name.c_str(), word.nameClass.c_str());
+            }
+        }
+//		fprintf(stderr,"pushWord(%s) class=%s\n", word.name.c_str(), word.nameClass.c_str());
 		words.push_back(word);
 		Word* wp = &words.back();
 		for (it = words.begin(); it != words.end(); ++it) {
-			WordPair pair(&*it, wp, &params);
-//			printf("pushWordPair(%s,%s)\n", pair.firstWord->name.c_str(), pair.secondWord->name.c_str());
-			if (pair.distances.size() > 0)
+//            fprintf(stderr,"Trying to print\n");
+//            fprintf(stderr, "First word %s, Second word %s\n", it->name.c_str(), word.name.c_str());
+			WordPair pair(*it, word, params);
+//			fprintf(stderr,"pushWordPair(%s,%s) distance size %d\n", pair.firstWord.name.c_str(), pair.secondWord.name.c_str(), pair.distances.size());
+			if (pair.distances.size() > 0) {
 				pairs.push(pair);
+            }
 		}
+//        fprintf(stderr,"Done with pushWord\n");
 	}
 	
 	void BallSearch::pushWord(string word)
 	{
 		Word w;
 		w.name = word;
-		string::size_type pos;
 		int x = 0;
 		int y = 0;
-		for (pos = 0; pos <= word.length(); ++pos) {
-			int c = pos < word.length() ? word[pos] : -1;
-			switch(c) {
+        string::reverse_iterator rit;
+        for (rit = word.rbegin(); rit != word.rend(); ++rit) {
+            char h = *rit;
+            switch(h) {
 				case 'm': --x; break;
 				case 'M': ++x; break;
 				case 'n': --y; break;
 				case 'N': ++y; break;
 				default: {
 					if (x != 0 || y != 0) {
-						w.matrix = w.matrix*constructT(params, x, y);
-						x=y=0;
+						w.matrix = constructT(params, x, y) * w.matrix;
+						x = y = 0;
 					}
-					if (c == 'g') {
-						w.matrix = w.matrix*g;
-					} else if (c == 'G') {
-						w.matrix = w.matrix*G;
+					if (h == 'g') {
+						w.matrix = g * w.matrix;
+					} else if (h == 'G') {
+						w.matrix = G * w.matrix;
 					}
 				}
 			}
 		}
+        if (x != 0 || y != 0) {
+            w.matrix = constructT(params, x, y) * w.matrix;
+        }
+//        XComplex a = w.matrix.a;
+//        XComplex b = w.matrix.b;
+//        XComplex c = w.matrix.c;
+//        XComplex d = w.matrix.d;
+//        fprintf(stderr, "Word: %s\n", w.name.c_str());
+//        fprintf(stderr, "At the center is has coords\n");
+//        fprintf(stderr, "a: %f + I %f\n", a.re, a.im);
+//        fprintf(stderr, "b: %f + I %f\n", b.re, b.im);
+//        fprintf(stderr, "c: %f + I %f\n", c.re, c.im);
+//        fprintf(stderr, "d: %f + I %f\n", d.re, d.im);
 		//printf("pushWord(%s): distance=%f\n", w.name.c_str(), 0.5 / norm(w.matrix.c));
 		pushWord(w);
 		Word wInv = inverse(w);
@@ -243,7 +271,7 @@ namespace BallSearchImpl {
 	
 	Word BallSearch::findWord()
 	{
-		for (;;) {
+		while (pairs.size() > 0) {
 			WordPair largest(pairs.top());
 			Word result(largest.currentCombination);
 			string oldName = result.name;
@@ -251,13 +279,13 @@ namespace BallSearchImpl {
 			pairs.pop();
 			largest.setCurrentIndex(largest.distanceIndex+1);
 			pairs.push(largest);
-			list<Word>::iterator it;
+			vector<Word>::iterator it;
 			for (it = words.begin(); it != words.end(); ++it)
 				if (result.nameClass == it->nameClass)
 					break;
 			if (result.nameClass != "" && it == words.end()) {
 				double sizeRatio = absLB(result.matrix.c) / absUB(words.front().matrix.c);
-				//fprintf(stderr, "%s: sizeRatio = %f gPower = %d\n", result.name.c_str(), sizeRatio, gPower(result));
+//				fprintf(stderr, "%s: sizeRatio = %f gPower = %d\n", result.name.c_str(), sizeRatio, gPower(result));
 				if (sizeRatio < 1) {
 					m_foundBigBall = true;
 				}
@@ -274,6 +302,7 @@ namespace BallSearchImpl {
 			//);
 			//printf("duplicate; resorting\n");
 		}
+        return Word();
 	}
 	
 	void BallSearch::addRelator(string w)
@@ -285,7 +314,15 @@ namespace BallSearchImpl {
 vector<string> findWords(Params<XComplex> center, vector<string> seedWords, int numWords, int maxLength,
 	vector<string> relators)
 {
+//    fprintf(stderr, "Params:\n");
+//    fprintf(stderr, "L: %f + I %f \n", center.lattice.re, center.lattice.im);
+//    fprintf(stderr, "S: %f + I %f \n", center.loxodromic_sqrt.re, center.loxodromic_sqrt.im);
+//    fprintf(stderr, "P: %f + I %f \n", center.parabolic.re, center.parabolic.im);
 	BallSearchImpl::BallSearch search(center);
+//    fprintf(stderr, "Params:\n");
+//    fprintf(stderr, "L: %f + I %f \n", search.params.lattice.re, search.params.lattice.im);
+//    fprintf(stderr, "S: %f + I %f \n", search.params.loxodromic_sqrt.re, search.params.loxodromic_sqrt.im);
+//    fprintf(stderr, "P: %f + I %f \n", search.params.parabolic.re, search.params.parabolic.im);
 	for (int i = 0; i < relators.size(); ++i)
 		if (relators[i].size() > 0)
 			search.addRelator(relators[i]);
@@ -296,8 +333,10 @@ vector<string> findWords(Params<XComplex> center, vector<string> seedWords, int 
 	search.pushWord("G");
 	vector<string> foundWords;
 	while (numWords > int(foundWords.size()) || (-numWords > int(foundWords.size()) && !search.foundBigBall())) {
+//        fprintf(stderr, "While loop\n");
 		BallSearchImpl::Word w = search.findWord();
-		if (gPower(w) > maxLength) {
+        int g_power = gPower(w);
+		if (g_power == 0 || g_power > maxLength) {
 			search.m_foundBigBall = false;
 			continue;
 		}
